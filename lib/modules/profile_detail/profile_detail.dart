16b24251cdb6bd170be14d0f4981f6b8301e9f53
dhikamp2301 <2006070@itg.ac.id>
2023-04-15 05:38:45 +0700

    add profile_detail

---
+++ lib/modules/profile_detail/profile_detail.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:pertemuan_v/models/user.dart';
+
+class profileDetail extends StatefulWidget {
+  const profileDetail({
+    super.key, 
+    required this.user,
+  });
+  final User user;
+
+  @override
+  State<profileDetail> createState() => _profileDetailState();
+}
+
+class _profileDetailState extends State<profileDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          "Profile Detail Screen",
+        ),
+      ),
+    );
+  }
+}
