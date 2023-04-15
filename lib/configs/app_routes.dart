c0d06b0f1cb9480ae7c6b58a7f322d3c4f6ab21b
dhikamp2301 <2006070@itg.ac.id>
2023-04-15 08:04:34 +0700

    add appRoutes

--- lib/configs/app_routes.dart
+++ lib/configs/app_routes.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:pertemuan_v/modules/home_screen/home_screen.dart';
 import 'package:pertemuan_v/modules/news_detail_screen/news_detail_screen.dart';
-import 'package:pertemuan_v/modules/profile_detail/profile.dart';
+import 'package:pertemuan_v/modules/profile_detail/profile_detail.dart';
 import 'package:pertemuan_v/modules/splash_screen/splash_screen.dart';
 
 import '../models/user.dart';
@@ -12,6 +12,20 @@ class AppRoutes {
   static const String home = "home";
   static const String newsDetail = "news-detail";
   static const String profileDetail = "profile-detail";
+
+  static Page _profileScreenBuilder(BuildContext context, GoRouterState state) {
+    late User user;
+    if (state.extra != null && state.extra is User) {
+      user = state.extra as User;
+    } else {
+      user = User.dummy();
+    }
+    return MaterialPage(
+      child: ProfileDetail(
+        user: user,
+      ),
+    );
+  }
 
   static Page _splahScreenBuilder(BuildContext context, GoRouterState state) {
     return const MaterialPage(
@@ -55,7 +69,7 @@ class AppRoutes {
           ),
           GoRoute(
             name: profileDetail,
-            path: "profile-Detail",
+            path: "profileDetail",
             pageBuilder: _profileScreenBuilder,
           ),
         ],
